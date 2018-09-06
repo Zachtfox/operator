c90ad90274645a73d8e0eda4524b5b2bdccee26f
Zach <zfox@students.mchenry.edu>
2018-09-06 11:07:31 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int intval1 = 5;
+        int intval2 = 5;
+        int result1 = intval1 + intval2;
+
+        int intval3 = 10;
+        int intval4 = 2;
+        int result2 = intval3 - intval4;
+
+        int intval5= 6;
+        int intval6 = 3;
+        int result3 = intval5 * intval6;
+
+        int intval7 = 12;
+        int intval8 = 4;
+        int result4 = intval7 / intval8;
+                ;
+        int intval9 = 10;
+        int intval10= 3;
+        int result5 = intval9 % intval10;
+
+        System.out.println(intval1 + " + "+ intval2 +" = " + result1 );
+
+        System.out.println(intval3 + " - "+ intval4 +" = " + result2 );
+
+        System.out.println(intval5 + " * "+ intval6 +" = " + result3 );
+
+        System.out.println(intval7 + " / "+ intval8 +" = " + result4 );
+
+        System.out.println(intval9 + " % "+ intval10 +" = " + result5 );
+    }
+}
